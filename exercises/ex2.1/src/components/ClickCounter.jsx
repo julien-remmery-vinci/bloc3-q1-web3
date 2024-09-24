5a9ea005667b35c9c17c6ba508ac85d17b56afa8
julien-remmery-vinci <julien.remmery@student.vinci.be>
2024-09-24 14:27:15 +0200

    new:ex2.1-stored

--- exercises/ex2.1/src/components/ClickCounter.jsx
+++ exercises/ex2.1/src/components/ClickCounter.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 
 const ClickCounter = ({ title, message }) => {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(JSON.parse(localStorage.getItem("count")));
     const [showMessage, setShowMessage] = useState(false);
     
+    const handleCount = () => {
+        setCount(count + 1);
+        localStorage.setItem("count", JSON.stringify(count + 1));
+    }
+
     return (
         <div>
             <h2>{title}</h2>
             <p>Count :{count}</p>
-            <button onClick={() => setCount(count + 1)} onMouseEnter={() => setShowMessage(true)} onMouseLeave={() => setShowMessage(false)}>
+            <button onClick={() => handleCount()} onMouseEnter={() => setShowMessage(true)} onMouseLeave={() => setShowMessage(false)}>
                 {showMessage ? '"Please click on me now !"' : 'Click'}
             </button>
-            {count >= 10 ? <p>{message}</p> : ''}
+            {count >= 10 && <p>{message}</p>}
         </div>
     );
 };
