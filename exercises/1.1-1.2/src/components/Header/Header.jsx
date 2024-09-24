23c25649913cb317aa561fc4aac25f9bb660fcdb
julien-remmery-vinci <julien.remmery@student.vinci.be>
2024-09-24 14:06:19 +0200

    new:ex2.1-init

--- exercises/1.1-1.2/src/components/Header/Header.jsx
+++ exercises/1.1-1.2/src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
+import './Header.css';
 import LOGO from './images/LOGO HE VINCI.png';
 
 const Header = (props) => {
   return (
-    <>
+    <div className='header'>
       <img src={LOGO} />
       <h1>{props.course}</h1>
-    </>
+    </div>
   );
 };
 
